13a97ca814a2f34544396bce8dbc6f9cc182ad77
stefan <stefan24122@gmail.com>
2022-08-17 23:44:12 +0200

    fixed one bug, at least one more remaining

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 // a circle of radius R1 centered at R2
 const float R1 = 1;
-const float R2 = 2;
+const float R2 = 1;
 
 
 // distance from the viewer to donut
@@ -17,31 +17,36 @@ const float K2 = 5;
 
 const int THETA_SPACING = 4;
 const int PHI_SPACING = 1;
-const float ROTATION_SPEED_A = 4;
-const float ROTATION_SPEED_B = 2;
-// const int SCREEN_WIDTH = 100;
-// const int SCREEN_HEIGHT = 100;
-const int SCREEN_WIDTH = 100;
-const int SCREEN_HEIGHT = 100;
+const int ROTATION_SPEED_A = 4;
+const int ROTATION_SPEED_B = 2;
+
+
+
+
+const int SCREEN_WIDTH = 30;
+const int SCREEN_HEIGHT = 30;
+
+
+
+
 
 // distance from eye to screen aka z'
-const float K1 = (float) SCREEN_WIDTH * K2 * 3 / (8*(R1+R2));
+const float K1 = ((float) SCREEN_WIDTH) * 3.0f / (8.0f*(R1+R2));
 
 
 int main() {
 
     // rotation
-    float A = 55;
-    float B = 55;
+    int A = 55;
+    int B = 55;
 
 
     // reciprocal z buffer (z⁽⁻¹⁾)
     // 0 means infinitely far away
-    // TODO update above comments
     float z_buff[SCREEN_HEIGHT][SCREEN_WIDTH];
     char out_buff[SCREEN_HEIGHT][SCREEN_WIDTH];
 
-    memset(z_buff, INFINITY, sizeof(float) * SCREEN_HEIGHT * SCREEN_WIDTH);
+    memset(z_buff, 0, sizeof(float) * SCREEN_HEIGHT * SCREEN_WIDTH);
     memset(out_buff, ' ', sizeof(char) * SCREEN_HEIGHT * SCREEN_WIDTH);
 
 #pragma clang diagnostic push
@@ -57,8 +62,9 @@ int main() {
             float theta = (float) _t;
 
 
-            float circleX = R2 + R1 * cosf((float) theta);
-            float circleY = R1 * sinf((float) theta);
+            float circleX = R2 + R1 * cosf( theta);
+            float circleY = R1 * sinf( theta);
+            float circleZ = 0;
 
             matrix4x4 torus = create_zero_matrix();
             torus.data[0][0] = circleX;
@@ -68,14 +74,12 @@ int main() {
             for (int _p = 0; _p < 360; _p += PHI_SPACING) {
                 float phi = (float) _p;
 
-                //int xp = (int) (SCREEN_WIDTH/2 + K1 * )
 
                 // rotate circle around the y-axis and
                 // rotate x-axis by A degrees
                 // rotate y-axis by B degrees
-                matrix4x4 rotator = rotate(A, (float) phi, B);
+                matrix4x4 rotator = rotate(A % 360, (float) phi, B % 360);
                 matrix4x4 xyz = m_times_n(&torus, &rotator);
-                //print_matrix(&xyz);
 
                 float x = xyz.data[0][0];
                 float y = xyz.data[0][1];
@@ -83,8 +87,8 @@ int main() {
 
                 // x and y projection
 
-                int xp = (int) ((float) SCREEN_WIDTH / 2 + K1 * x / z);
-                int yp = (int) ((float) SCREEN_HEIGHT / 2 - K1 * y / z);
+                int xp = K1 * x / (K2 + z);
+                int yp = (int) ((float)SCREEN_HEIGHT - K1 * y / (K2 + z));
 
                 // create surface normal
                 matrix4x4 surface_normal = create_zero_matrix();
@@ -93,29 +97,32 @@ int main() {
 
                 // create light and calculate luminance by multiplying it with surface normal
                 matrix4x4 light = create_zero_matrix();
-                //light.data[1][0] = 1 / sqrtf(2);
+                //light.data    -=+ 1 / sqrtf(2);
                 light.data[1][0] = 0.70710678118f;
                 light.data[2][0] = -0.70710678118f;
+
                 float luminance = m_times_n(&surface_normal, &light).data[0][0];
 
                 if (luminance > 0) {
-                    printf("test");
-                    //if(1/z > z_buff[yp][xp]) {
-                    if (z < z_buff[yp][xp]) {
+                    //print_matrix(&xyz);
+                    if (1/z > z_buff[yp][xp]) {
                         int luminance_index = (int) (10.0f * luminance);
                         out_buff[yp][xp] = ".,.~:;=!#$@"[luminance_index];
                     }
                 }
 
-                printf("\x1b[H");
-                for (int i = 0; i < SCREEN_HEIGHT; i++) {
-                    for (int j = 0; j < SCREEN_WIDTH; j++) {
-                        putchar(out_buff[j][i]);
-                    }
-                    putchar('\n');
-                }
+
 
             }
+
+            printf("\x1b[H");
+            for (int i = 0; i < SCREEN_HEIGHT; i++) {
+                for (int j = 0; j < SCREEN_WIDTH; j++) {
+                    putchar(out_buff[j][i]);
+                }
+                putchar('\n');
+            }
+            //break;
         }
 
         // spin the donut around the x and z axes
